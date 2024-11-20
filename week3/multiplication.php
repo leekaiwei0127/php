9b926474352e8faa3002e9fcb8f5f26248d60c44
LDxiaokai <leekaiwei0127@e.neivce.edu.my>
2024-11-20 15:17:07 +0800

    classtest

---
+++ week3/multiplication.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+
+    $number = 5;
+
+    for ($i = 1; $i <= 10; $i++) {
+        $product = $number * $i;
+        echo "$number x $i = $product<br>";
+    }
+    ?>
+</body>
+
+</html>
---
+++ week3/sum.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+
+    $sum = 0;
+
+
+    for ($i = 1; $i <= 10; $i++) {
+        $sum += $i;
+    }
+
+
+    echo "Sum of first 10 natural numbers is: " . $sum;
+    ?>
+</body>
+
+</html>
