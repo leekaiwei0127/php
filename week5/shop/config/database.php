96d1d777b2671df4a2d18b2701202b949337961e
LDxiaokai <leekaiwei0127@e.neivce.edu.my>
2024-12-18 09:18:10 +0800

    classtest

---
+++ week5/shop/config/database.php
@@ -0,0 +1,14 @@
+<?php
+// used to connect to the database
+$host = "localhost";
+$db_name = "online_store";
+$username = "root";
+$password = "";
+
+//Error Handling
+try {
+    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+    echo "Connected successfully";
+} catch (PDOException $exception) {
+    echo "Connection error: " . $exception->getMessage();
+}
---
+++ week5/shop/customer_create.php
@@ -0,0 +1,137 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</head>
+
+<body>
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Create Customer</h1>
+        </div>
+        <?php
+        if ($_POST) {
+            // include database connection
+            include 'config/database.php';
+            try {
+                // posted values
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+                $firstname = $_POST['firstname'];
+                $lastname = $_POST['lastname'];
+                $gender = $_POST['gender'];
+                $dob = $_POST['dateofbirth'];
+                $errors = [];
+                //Check Username
+                if (empty($username)) {
+                    $errors[] = 'Username is required.';
+                }
+                //Check Password
+                if (empty($password)) {
+                    $errors[] = 'Password is required.';
+                }
+                //Check First Name
+                if (empty($firstname)) {
+                    $errors[] = 'First Name is required.';
+                }
+                //Check Last Name
+                if (empty($lastname)) {
+                    $errors[] = 'Last Name is required.';
+                }
+                //Check Gender
+                if (empty($gender)) {
+                    $errors[] = 'Gender is required.';
+                }
+                //Check Date Of Birth
+                if (empty($dob)) {
+                    $errors[] = 'Date of birth is empty.';
+                }
+                //If there is errors, show them
+                if (!empty($errors)) {
+                    echo "<div class='alert alert-danger'><ul>";
+                    foreach ($errors as $error) {
+                        echo "<li>{$error}</li>";
+                    }
+                    echo "</ul></div>";
+                } else {
+                    // insert query
+                    $query = "INSERT INTO customer SET username=:username, password=:password, firstname=:firstname, lastname=:lastname, 
+                    gender=:gender, date_of_birth=:date_of_birth";
+                    // prepare query for execution
+                    $stmt = $con->prepare($query);
+                    // bind the parameters
+                    $stmt->bindParam(':username', $username);
+                    $stmt->bindParam(':password', $password);
+                    $stmt->bindParam(':firstname', $firstname);
+                    $stmt->bindParam(':lastname', $lastname);
+                    $stmt->bindParam(':gender', $gender);
+                    $stmt->bindParam(':date_of_birth', $dob);
+                    // specify when this record was inserted to the database
+                    // Execute the query
+                    if ($stmt->execute()) {
+                        echo "<div class='alert alert-success'>Product was added.</div>";
+                    } else {
+                        echo "<div class='alert alert-danger'>Unable to save record.</div>";
+                    }
+                }
+            }
+            // show error
+            catch (PDOException $exception) {
+                die('ERROR: ' . $exception->getMessage());
+            }
+        }
+        ?>
+
+        <!-- html form to create product will be here -->
+
+        <form action='<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>' method="post">
+            <table class='table table-hover table-responsive table-bordered'>
+                <tr>
+                    <td>Username</td>
+                    <td><input type='text' name='username' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input type="password" name='password' class='form-control'></input></td>
+                </tr>
+                <tr>
+                    <td>First Name</td>
+                    <td><input type='text' name='firstname' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Last Name</td>
+                    <td><input type='text' name='lastname' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Gender</td>
+                    <td>
+                        <input type='radio' id='male' name='gender' value='male' />
+                        <label for="male">Male</label><br>
+                        <input type='radio' id='female' name='gender' value='female' />
+                        <label for="female">Female</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Date of birth</td>
+                    <td><input type='date' name='dateofbirth' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' value='Save' class='btn btn-primary' />
+                        <a href='index.php' class='btn btn-danger'>Back to read products</a>
+                    </td>
+                </tr>
+            </table>
+        </form>
+
+    </div>
+    <!-- end .container -->
+</body>
